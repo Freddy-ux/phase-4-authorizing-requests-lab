b977c8436179084d8deb6a84e84299567ed8b4f9
Freddy-ux <alfredmuleri569@gmail.com>
2023-06-30 00:46:35 +0300

    Done

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,20 +1,40 @@
 class MembersOnlyArticlesController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :require_login
 
   def index
-    articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
-    render json: articles, each_serializer: ArticleListSerializer
+    # Return the JSON data for the members-only articles
+    render json: { articles: members_only_articles }
   end
 
   def show
+    # Find the article by ID
     article = Article.find(params[:id])
-    render json: article
+
+    if article.is_member_only
+      # Return the JSON data for the members-only article
+      render json: { article: article }
+    else
+      # Return a 401 unauthorized status code and an error message
+      render json: { error: "Unauthorized access to this article" }, status: 401
+    end
   end
 
   private
 
-  def record_not_found
-    render json: { error: "Article not found" }, status: :not_found
+  def require_login
+    unless logged_in?
+      # Return a 401 unauthorized status code and an error message
+      render json: { error: "Unauthorized access. Please log in." }, status: 401
+    end
   end
 
+  def logged_in?
+    # Check if the user is logged in based on the session[:user_id] presence
+    session[:user_id].present?
+  end
+
+  def members_only_articles
+    # Retrieve the members-only articles from the database
+    Article.where(is_member_only: true)
+  end
 end
